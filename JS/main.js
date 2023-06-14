ef9ff711567b29bc6e6ce1ec41b6564c759676cb
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-14 10:34:39 -0400

    centered box in page, looking up how to connect api, maybe go back to that later, dont understand what I did

--- JS/main.js
+++ JS/main.js
@@ -7,7 +7,15 @@ function currentWeatherData() {
 
 
   fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}`)
-
+    .then(response => response.json())
+    .then(data => {
+      
+        console.log(data);
+    })
+    .catch(error => {
+        console.log('Error:', error);
+    });
+    
     var zipCode = document.getElementById("zipCodeInput").value;
     console.log("Zip Code:", zipCode);
   }

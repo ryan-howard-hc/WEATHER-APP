f6e22b9b3d4d60ba8deea4ef7da1a5d85f3e2b54
Ryan Howard <133996155+ryan-howard-hc@users.noreply.github.com>
2023-06-14 11:22:51 -0400

    learning how to what JSON is and how to fetch data into a table directly based on the raw data

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,18 @@
-
-
-
 function currentWeatherData() {
-  var apiKEY= 'f91a2ba49ec43ee8f836bbbd73a614e7';
-  var zipCode= document.getElementById("zipCodeInput").value;
-
+  var apiKey = 'f91a2ba49ec43ee8f836bbbd73a614e7';
+  var zipCode = document.getElementById("zipCodeInput").value;
 
   fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${apiKey}`)
-    .then(response => response.json())
-    .then(data => {
-      
-        console.log(data);
-    })
-    .catch(error => {
-        console.log('Error:', error);
-    });
-    
-    var zipCode = document.getElementById("zipCodeInput").value;
-    console.log("Zip Code:", zipCode);
-  }
+      .then(response => response.json())
+      .then(data => {
+          var table = createWeatherTable(data);
+          var weatherDataDiv = document.getElementById("weatherData");
+          weatherDataDiv.innerHTML = table;
+          weatherDataDiv.style.display = "block";
+      })
+      .catch(error => {
+          console.log('Error:', error);
+      });
 
+  console.log("Zip Code:", zipCode);
+}
